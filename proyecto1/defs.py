22475bb9b44ab86e7a28813a081cdfcb483416f0
DaveAlsina <davealsina@gmail.com>
2022-02-28 17:51:55 -0500

    pre pruebas de interfaz

--- proyecto1/defs.py
+++ proyecto1/defs.py
@@ -4,9 +4,10 @@ import numpy as np
 import cv2
 
 
-def BlurFilter(img, filter_sz=None, blur_weights=None):
+def blurFilter(img, filter_sz=None, blur_weights=None):
     if blur_weights is None:
         blur_weights = np.ones(filter_sz)/filter_sz
+    
     img = convolve1d(img, weights=blur_weights, axis=0)
     img = convolve1d(img, weights=blur_weights, axis=1)
     return img
@@ -18,27 +19,53 @@ BLUR30 = np.ones(30) / 30
 
 SOBEL_KERNELX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 SOBEL_KERNELY = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+LAPLACIAN_KERNEL = np.array([[0,1,0],[1,-4,1],[0,1,0]])
 
 SOBEL_KERNEL_0 = np.array([1, 2, 1])
 SOBEL_KERNEL_1 = np.array([-1, 0, 1])
 
 
-def SobelFilter(img):
-    img = BlurFilter(img, blur_weights=BLUR3)
+
+def sobelFilter(img, normalize = True):
+
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    border_X = convolve2d(img, SOBEL_KERNELX)
-    border_Y = convolve2d(img, SOBEL_KERNELY)
+    border_X = convolve2d(img, SOBEL_KERNELX,
+                          mode='same', boundary='fill')
+    border_Y = convolve2d(img, SOBEL_KERNELY,
+                          mode='same', boundary='fill')
 
     convolved_img = border_X + border_Y
-    convolved_img = ((convolved_img - np.mean(convolved_img)) /
-                     (np.var(convolved_img)))
-    return (255*convolved_img).astype('uint8')
+
+    if normalize:
+        convolved_img = ((convolved_img - np.mean(convolved_img)) / (np.var(convolved_img)))
+        return (255*convolved_img).astype('uint8')
+
+    else:
+        return convolved_img.astype('uint8')
+
+
+def laplaceFilter(img, normalize = True):
+
+    #convierte la imagen a escala de grises
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    #aplica el filtro del kernel de laplace
+    convolved_img = convolve2d(gray, LAPLACIAN_KERNEL,
+                               mode='same', boundary='fill')
+
+    if normalize:
+        convolved_img = (convolved_img -np.mean(convolved_img))/(np.var(convolved_img))
+        return (255*convolved_img).astype('uint8')
+
+    else:
+        return convolved_img.astype('uint8')
+
 
 
 def equalize_hsv_channel(img, channel=2):
+
     n, m, _ = np.shape(img)
-    img = BlurFilter(img, blur_weights=BLUR3)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
     sat = img[:, :, channel]
@@ -48,4 +75,40 @@ def equalize_hsv_channel(img, channel=2):
     mapping[unique] = counts
     mapping = 255/(m*n)*np.cumsum(mapping)
     img[:, :, channel] = mapping[sat]
+
     return cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+
+
+def sharpness_sobel(img):
+    
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    borders = sobelFilter(img, normalize=False)
+    
+    img_hsv[:,:,2] = img_hsv[:,:,2] - borders
+    print(np.max(img_hsv[:,:,2]))    
+    print(np.min(img_hsv[:,:,2]))
+    
+    output = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB)
+
+    return output.astype('uint8')
+
+
+def sharpness_laplace(img):
+    
+    #obtiene los bordes de la imagen usando 
+    #el kernel de laplace
+    borders = laplaceFilter(img, False)
+    
+    #convierte la imagen a HSV para quedarse con 
+    #'value' de la matriz y aumentarle el sharpness
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    img_hsv[:,:,2] = img_hsv[:,:,2] - borders
+    
+    #convierte de regreso a RGB
+    output = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB)
+    
+    return output.astype('uint8')
+
+
+
+
--- proyecto1/gui.py
+++ proyecto1/gui.py
@@ -1,8 +1,9 @@
 import cv2
 
-from defs import SobelFilter, equalize_hsv_channel, BlurFilter, BLUR30
+from defs import sobelFilter, equalize_hsv_channel, blurFilter, BLUR30
 
 if __name__ == "__main__":
+
     vid = cv2.VideoCapture(0)
     while(True):
 
@@ -11,11 +12,11 @@ if __name__ == "__main__":
         ret, frame = vid.read()
 
         ''' Border Detection '''
-        # cv2.imshow('frame', SobelFilter(frame))
+        # cv2.imshow('frame', sobelFilter(frame))
         '''Histogram Equalization'''
         # cv2.imshow('frame', equalize_hsv_channel(frame, 2))
         ''' Blurring Effect '''
-        cv2.imshow('frame', BlurFilter(frame, blur_weights=BLUR30))
+        cv2.imshow('frame', blurFilter(frame, blur_weights=BLUR30))
 
         # the 'q' button is set as the
         # quitting button you may use any
